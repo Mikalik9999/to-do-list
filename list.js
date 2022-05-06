7dbd5e231f1ff28f6d23f11a97deee6cc1b85e39
Vadim <www.vadim.2@mail.ru>
2022-05-06 15:52:59 +0300

    1.0

--- list.js
+++ list.js
@@ -1,53 +1,43 @@
-// Создайте кнопку "закрыть" и добавьте ее к каждому элементу списка
-const myNodelist = document.getElementsByTagName("LI");
-for (i = 0; i < myNodelist.length; i++) {
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+close()
+function vvod() {
+    const input = document.querySelector("input[type='text']");
+    const ul = document.querySelector("ul.todos");
+    function createTodo() {
+        let parent = document.querySelector('#li');
+        let p = document.createElement('li');
+        const input = document.querySelector("input[type='text']");
+        p.innerHTML = input.value;
+        parent.append(p);
+    }
+    input.addEventListener("keypress", (keyPressed) => {
+        const keyEnter = 13;
+        if (keyPressed.which === keyEnter) {
+            createTodo();
+            close();
+        }
+    });
+    ul.addEventListener("click", onClickTodo);
+    input.value = "";
 }
+vvod()
 
-// Нажмите на кнопку закрыть, чтобы скрыть текущий элемент списка
-const close = document.getElementsByClassName("close");
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        let div = this.parentElement;
-        div.style.display = "none";
+function close(){
+    let myClose = document.getElementsByTagName("LI");
+    let i;
+    for (i = 0; i < myClose.length; i++) {
+        let span = document.createElement("SPAN");
+        let txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        myClose[i].appendChild(span);
     }
-}
-
-// Добавление символа "проверено" при нажатии на элемент списка
-const list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
-// Создайте новый элемент списка при нажатии на кнопку "Добавить"
-function newElement() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("myInput").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("Вы должны что-то написать!");
-    } else {
-        document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            let div = this.parentElement;
+    const close = document.getElementsByClassName("close");
+    let a;
+    for (a = 0; a < close.length; a++) {
+        close[a].onclick = function() {
+            const div = this.parentElement;
             div.style.display = "none";
         }
     }
 }
+
